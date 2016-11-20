63d34e13a0904b355012d15211177f4e457ac485
Swapneel Chalageri <swapneel96@gmail.com>
2016-11-20 09:18:43 -0500

    Add files via upload

--- rle.c
+++ rle.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
-char *encode(char *src){
+int * partitionInput(int numPartitions, int length);
+
+
+struct arg_struct {
+    int arg1;
+    int arg2;
+    char * string;
+};
+
+
+
+void *encode(void  *arg){
+
+	struct arg_struct *args = arg;
+
+	char *src = (char*)args->string;
 	
 	int rlen; 
 	char count[256];
@@ -13,8 +29,11 @@ char *encode(char *src){
 	int i;
 	int k;
 	int j = 0;
+	
+	printf("%d\n", args->arg1);
+	printf("%d\n", args->arg2);	
 
-	for (i = 0; i < len; i++){
+	for (i = args->arg1; i < args->arg2; i++){
 
 		dest[j++] = src[i];
 
@@ -35,23 +54,72 @@ char *encode(char *src){
 	}
 
 	dest[j] = '\0';
-	return dest;
+	printf("%s\n", dest);
+	return NULL;
 }
 
 
 int main(){
 
 	char str[] = "heeppppopopeeeelllloooo";
-	char *res = encode(str);
-	printf("%s", res);
-	getchar();
+
+	int * values = partitionInput(5, strlen(str));
+	
+	printf("%d\n", values[0]);
+	printf("%d\n", values[1]);
+	printf("%d\n", values[2]);
+	printf("%d\n", values[3]);
+	printf("%d\n", values[4]);
+
+	printf("%lu\n", strlen(str));	
+	
+	pthread_t pth;
+	int i = 0;
+
+	struct arg_struct args; 
+	
+	for (i = 0; i < 4; i++){
+	
+    		args.arg1 = values[i];
+    		args.arg2 = values[i]-1;
+		args.string = str;
+		pthread_create(&pth, NULL, encode, (void*)&args);
+		
+	}
+
+	pthread_join(pth, NULL);
+	return 0;
+	
+		
 }
 
+int * partitionInput(int numPartitions, int length) {
 
+        int partitionSize = length/numPartitions;
+        int remainder = length % numPartitions;
 
+        int * partitionIndices = (int *)malloc(sizeof(int)*numPartitions);
+        partitionIndices[0] = 0;
 
+        int i;
 
+        for (i = 0; i < numPartitions - 1; i++) {
 
+                if (i == 0) {
 
+                        partitionIndices[1] = partitionSize + remainder;
 
+                }
+
+                else {
+
+                        partitionIndices[i + 1] = partitionIndices[i] + partitionSize;
+
+                }
+
+        }
+
+        return partitionIndices;
+
+}
 
